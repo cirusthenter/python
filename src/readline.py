437308d3cedf59535b86200b915725a50beed430
snakano <snakano@student.42tokyo.jp>
2020-04-25 20:07:06 +0900

    initial commit

---
+++ src/readline.py
@@ -0,0 +1,8 @@
+x, y, z, k = map(int, input().split())
+a = [int(i) for i in input().split()]
+b = [int(i) for i in input().split()]
+c = [int(i) for i in input().split()]
+print('x:', x, 'y:', 'z:', z, 'k:', k)
+print('a:', a)
+print('b:', b)
+print('c:', c)
---
+++ src/squares.py
@@ -0,0 +1,4 @@
+squares = []
+for i in range(10):
+    squares.append(i ** 2)
+print(squares)
